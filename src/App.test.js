2796e03603950ca9100ca11ecfcf761f615685fb
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-05-01 23:06:59 -0700

    revising tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { mount } from '@cypress/react'
+import Home from './components/Home'
 import App from './App'
 
+// Home Component Tests
+
 it('renders learn react link', () => {
-  mount(<App />)
+  mount(<Home />)
   cy.get('.App').contains('Hello Cypress')
 })
 
 it('checks to see if the button can be clicked and if the alert triggers', () => {
-  mount(<App />)
+  mount(<Home />)
   cy.get('.button').click()
 })
+
+
